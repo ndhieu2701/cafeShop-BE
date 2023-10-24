fc762e3a0deab88ceb5dd4783fe1fea7242361c3
hieundAmela <139426235+hieundAmela@users.noreply.github.com>
2023-10-24 17:32:21 +0700

    feature: create new models, apis

--- apis/index.js
+++ apis/index.js
@@ -1,7 +1,11 @@
 import userRoute from "./user.js";
+import categoryRoute from "./category.js"
+import tagRoute from "./tag.js"
 
 const route = (app) => {
   app.use("/user", userRoute);
+  app.use("/categories", categoryRoute)
+  app.use("/tags", tagRoute)
 };
 
 export default route;
